eee0e97ded4796aa962e6adc83e0169cebb08202
Jeremy Cowelchuk <cowelch1@cse.msu.edu>
2020-02-10 22:51:29 -0500

    Add a Background.h file to handle the CBackground class used to draw the background and tile it correctly.  This could be derived from a class later.  Wanted to get working skeleton up first.

---
+++ Project1/Background.h
@@ -0,0 +1,14 @@
+#include <memory>
+#pragma once
+class CBackground
+{
+public:
+	CBackground();
+	void OnDraw(Gdiplus::Graphics* graphics, double scrollX);
+	double GetImageHeight() {return mImage->GetHeight(); };
+
+private:
+	Gdiplus::Bitmap* mImage;
+	float mScale;
+};
+
