6127398aaee8893685e8f0d7858cb6db0ec559af
Jeremy Cowelchuk <cowelch1@cse.msu.edu>
2020-02-10 22:49:44 -0500

    Add a Background.cpp file to handle functions related to the CBackground used for drawing the background.  The last three lines need to be reviewed prior to further work, but exist as an explanation of how they tile with 1/2 image overlap off the left side of the screen.

---
+++ Project1/Background.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "Background.h"
+
+using namespace Gdiplus;
+
+CBackground::CBackground()
+{
+    mImage = Bitmap::FromFile(L"../images/background.png");
+    if (mImage->GetLastStatus() != Ok)
+    {
+        AfxMessageBox(L"Failed to open images/background.png");
+    }
+}
+
+void CBackground::OnDraw(Gdiplus::Graphics* graphics,double scrollX)
+{
+    int wid = mImage->GetWidth();
+    int hit = mImage->GetHeight();
+
+    // draw the background tiled over a distance.  This needs to be figured out more
+    depending on the length of the level.
+    graphics->DrawImage(mImage, -wid/2 + scrollX, 0, wid, hit);
+    graphics->DrawImage(mImage, wid/2-1 + scrollX, 0, wid, hit);
+    graphics->DrawImage(mImage, 1.5 * wid - 2 + scrollX, 0, wid, hit);
+}
