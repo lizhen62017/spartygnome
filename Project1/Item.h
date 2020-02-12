4120c8fee0f2368354cd776f5d1eef93f1750121
Torre Rocco <roccotor@msu.edu>
2020-02-12 01:43:48 -0500

    Created Absract Item class with some getters/setters

--- Project1/Item.h
+++ Project1/Item.h
@@ -9,11 +9,73 @@
 #pragma once
 
 
+#include <memory>
+#include <string>
+
+class CLevel;
+
 /**
- * Base class representing an item in the level
+ * Base class for any item in our aquarium.
  */
 class CItem
 {
+public:
+	virtual ~CItem();
 
+	/// Default constructor (disabled)
+	CItem() = delete;
+
+	/// Copy constructor (disabled)
+	CItem(const CItem&) = delete;
+
+	/** The X location of the item
+	* \returns X location in pixels */
+	double GetX() const { return mX; }
+
+	/** The Y location of the item
+	* \returns Y location in pixels */
+	double GetY() const { return mY; }
+
+	/** The Aquarium the item is in
+	* \returns Aquarium the item is associated with */
+	CLevel* GetLevel() { return mLevel; }
+
+	/// Set the item location
+	/// \param x X location
+	/// \param y Y location
+	void SetLocation(double x, double y) { mX = x; mY = y; }
+
+	/// Draw this item
+	/// \param graphics Graphics device to draw on
+	void Draw(Gdiplus::Graphics* graphics);
+
+	/// Handle updates for animation
+	/// \param elapsed The time since the last update
+	virtual void Update(double elapsed) {}
+
+	/// Get the width of the item
+	/// \returns item width
+	int GetWidth() const { return mItemImage->GetWidth(); }
+
+	/// Get the height of the item
+	/// \returns item height
+	int GetHeight() const { return mItemImage->GetHeight(); }
+
+protected:
+	CItem(CLevel* level, const std::wstring& filename);
+
+private:
+	/// The level this item is contained in
+	CLevel* mLevel;
+
+	// Item location in the level
+	double  mX = 0;     ///< X location for the center of the item
+	double  mY = 0;     ///< Y location for the center of the item
+
+	/// The image of this item
+	std::unique_ptr<Gdiplus::Bitmap> mItemImage;
+
+	/// id given to specific item if it needs to be copied
+	std::wstring mID;
 };
 
