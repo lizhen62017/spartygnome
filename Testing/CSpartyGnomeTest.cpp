fdf1f3ce59161e84000e3d4bb57cc12b8af6181f
Robert Guskte <gustkero@msu.edu>
2020-02-13 21:57:28 -0500

    Added to Gnome testing

--- Testing/CSpartyGnomeTest.cpp
+++ Testing/CSpartyGnomeTest.cpp
@@ -20,13 +20,28 @@ namespace Testing
 		TEST_METHOD(TestLocation)
 		{
 			CSpartyGnome gnome;
-
+			gnome.SetLocX(512);
+			gnome.SetLocY(512);
 			Assert::IsTrue(gnome.GetX() == 512, L"Correct Test");
 			Assert::IsTrue(gnome.GetY() == 512, L"Correct Test");
 			gnome.SetLocX(600);
 			gnome.SetLocY(700);
 			Assert::IsFalse(gnome.GetX() == 512, L"Incorrect Test");
 			Assert::IsFalse(gnome.GetY() == 512, L"Incorrect Test");
+		}
+
+		TEST_METHOD(TestVelocity)
+		{
+			CSpartyGnome gnome;
+
+			Assert::IsTrue(gnome.GetVelX() == 0, L"Correct Test");
+			Assert::IsTrue(gnome.GetVelY() == 0, L"Correct Test");
+			gnome.SetVelX(600);
+			gnome.SetVelY(700);
+			Assert::IsTrue(gnome.GetVelX() == 600, L"Correct Test");
+			Assert::IsTrue(gnome.GetVelY() == 700, L"Correct Test");
+			Assert::IsFalse(gnome.GetVelX() == 512, L"Incorrect Test");
+			Assert::IsFalse(gnome.GetVelY() == 512, L"Incorrect Test");
 		}
 
 	};
