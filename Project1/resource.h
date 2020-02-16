5e07712fb5622f86b078726242e1d3aa3e3ccf42
Chase Lindeboom <lindeboo@msu.edu>
2020-02-16 00:13:58 -0500

    Made an ItemVisitor Class

--- Project1/resource.h
+++ Project1/resource.h
@@ -2,18 +2,23 @@
 // Microsoft Visual C++ generated include file.
 // Used by Project1.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_Project1TYPE				130
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_Project1TYPE                130
+#define ID_LEVEL                        32771
+#define ID_LEVEL_LEVEL0                 32772
+#define ID_LEVEL_LEVEL1                 32773
+#define ID_LEVEL_LEVEL2                 32774
+#define ID_LEVEL_LEVEL3                 32775
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
