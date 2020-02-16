d54b1a6eea7f817507479883fa9f8315745f7cdf
Jeremy Cowelchuk <cowelch1@msu.edu>
2020-02-16 02:06:39 -0500

    Added empty skeleton class, "Wings" for TeamItem

---
+++ Project1/Wings.cpp
@@ -0,0 +1,8 @@
+/**
+ * \file Wings.cpp
+ *
+ * \author Jeremy Cowelchuk
+ */
+
+#include "pch.h"
+#include "Wings.h"
---
+++ Project1/Wings.h
@@ -0,0 +1,19 @@
+/**
+ * \file Wings.h
+ *
+ * \author Jeremy Cowelchuk
+ *
+ * 
+ */
+
+
+#pragma once
+#include "ItemPickup.h"
+/**
+ * Class for handling the team item, a wings item that allows double jumps
+ */
+class CWings :
+	public CItemPickup
+{
+};
+
